68ef4c7df1ba720d9ef302618e5b4e007fb7fb74
qwaezrx <hongjw0309@gmail.com>
2023-11-22 01:27:51 +0900

    image generated

---
+++ generate_image.py
@@ -0,0 +1,15 @@
+from diffusers import DiffusionPipeline
+import torch
+
+lora_path = "checkpoints/minsuck_checkpoints/checkpoint-1000"
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    torch_dtype=torch.float16,
+)
+
+pipe.to("cuda")
+pipe.load_lora_weights(lora_path)
+
+prompt = "A photo of zwc cat"
+image = pipe(prompt, num_inference_steps=30, guidance_scale=7.5).images[0]
+image.save("outputs/minsuck.png")
