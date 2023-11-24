fe5ffa43d13e4a724d392b58a1ca066b64ae29c8
qwaezrx <hongjw0309@naver.com>
2023-11-25 00:57:08 +0900

    .,

--- webui.py
+++ webui.py
@@ -140,7 +140,10 @@ accelerate launch train_dreambooth_lora_sdxl.py \
   --seed=0 \
   --checkpoints_total_limit=5
 """
-    train_input_args = [arg in train_command.split(" ")[3:] if arg]
+    train_input_args = []
+    for arg in train_command.split(" ")[3:]:
+        if arg:
+            train_input_args.append(arg)
 
     print("Input args:", train_input_args)
 
