89b8bc0f930862ff8db1aae1410114ac7e4ed719
qwaezrx <hongjw0309@naver.com>
2023-11-25 01:08:15 +0900

    .,

--- webui.py
+++ webui.py
@@ -156,7 +156,7 @@ accelerate launch train_dreambooth_lora_sdxl.py \
         "--train_batch_size=1",
         "--gradient_accumulation_steps=4",
         "--learning_rate=1e-4",
-        "--lr_schedular=constant",
+        "--lr_scheduler=constant",
         "--lr_warmup_steps=0",
         f"--checkpointing_steps={CHECKPOINTING_STEPS}",
         f"--max_train_steps={MAX_TRAIN_STEP}",
